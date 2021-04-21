d49a864aed06913bb18daf97542f616d2bab223a
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-04-21 19:10:14 -0400

    added footer info

--- src/components/Footer/FooterContainer.js
+++ src/components/Footer/FooterContainer.js
@@ -4,7 +4,24 @@ import './Footer.css'
 const FooterContainer = () => {
   return (
     <div className="footer-container">
-      Footer Container
+      <div className="footer-left">
+        <p className="footer-hours">
+          Hours of Operation<br />
+          M-F 10am - 8pm
+          </p>
+      </div>
+      <div className="footer-center">
+
+      </div>
+      <div className="footer-right">
+        <p className="footer-address">
+          Street address<br />
+          State, City Zip<br />
+          Phone
+          Email
+
+        </p>
+      </div>
     </div>
   )
 }
